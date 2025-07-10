89e240ebc4e0c64a906b34d5d2c4de57581633f4
X4X0 <craxterpy@gmail.com>
2025-07-10 13:27:58 -0400

    Update kernel_loader.asm

--- bootloader/kernel_loader.asm
+++ bootloader/kernel_loader.asm
@@ -1,1 +1,30 @@
+[BITS 16]
 
+load_stage2:
+    pusha
+    
+    mov ah, 0x00
+    mov dl, 0x80
+    int 0x13
+    
+    mov ah, 0x02
+    mov al, 4
+    mov ch, 0
+    mov cl, 2
+    mov dh, 0
+    mov dl, 0x80
+    mov bx, 0x1000
+    mov es, bx
+    mov bx, 0x0000
+    int 0x13
+    
+    jc disk_error
+    popa
+    ret
+
+disk_error:
+    mov si, disk_err_msg
+    call print_string
+    hlt
+
+disk_err_msg: db 'Disk read error!', 0
