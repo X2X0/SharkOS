4b94469e1a72545a08db8b16af8edae9720c5b04
X4X0 <craxterpy@gmail.com>
2025-07-10 14:00:01 -0400

    Update print.asm

--- bootloader/print.asm
+++ bootloader/print.asm
@@ -3,12 +3,27 @@
 print_string:
     pusha
 .loop:
+    lodsb                   ; Load byte from SI
+    test al, al             ; Check for null terminator
+    jz .done
+    mov ah, 0x0E            ; BIOS teletype
+    mov bh, 0x00            ; Page number
+    mov bl, 0x07            ; Color (light gray)
+    int 0x10
+    jmp .loop
+.done:
+    popa
+    ret
+
+print_string_16:
+    pusha
+.loop:
     lodsb
     test al, al
     jz .done
     mov ah, 0x0E
     mov bh, 0x00
-    mov bl, 0x07
+    mov bl, 0x0F            ; Bright white
     int 0x10
     jmp .loop
 .done:
