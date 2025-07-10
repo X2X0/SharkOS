7340df68e78ebc0c83879a4fdb8843005fd7f4f9
X4X0 <craxterpy@gmail.com>
2025-07-10 13:59:05 -0400

    Update animation.asm

--- bootloader/animation.asm
+++ bootloader/animation.asm
@@ -2,25 +2,29 @@
 
 shark_animation:
     pusha
-    mov cx, 5
+    mov cx, 5               ; 5 letters in SHARK
     mov bx, shark_frames
     
 .animate_loop:
     push cx
     push bx
     
+    ; Clear screen
     mov ax, 0x0003
     int 0x10
     
+    ; Set cursor position (center screen)
     mov ah, 0x02
     mov bh, 0x00
-    mov dh, 10
-    mov dl, 35
+    mov dh, 10              ; Row 10
+    mov dl, 35              ; Column 35
     int 0x10
     
+    ; Print current frame
     mov si, bx
     call print_string
     
+    ; Delay for animation effect
     mov cx, 0x05
     mov dx, 0x0000
 .delay:
@@ -30,7 +34,7 @@ shark_animation:
     
     pop bx
     pop cx
-    add bx, 2
+    add bx, 2               ; Next frame pointer
     loop .animate_loop
     
     popa
