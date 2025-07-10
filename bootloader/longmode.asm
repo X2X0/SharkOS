6f5ba0030a377b17f7231708fe3ab42c8f2c9a13
X4X0 <craxterpy@gmail.com>
2025-07-10 14:07:33 -0400

    Update longmode.asm

--- bootloader/longmode.asm
+++ bootloader/longmode.asm
@@ -1,4 +1,3 @@
-
 ; Page tables (must be 4KB aligned)
 align 4096
 pml4_table:
@@ -9,3 +8,24 @@ pdp_table:
 
 pd_table:
     resb 4096
+
+; =============================================================================
+; 64-bit Long Mode Entry Point
+; =============================================================================
+[BITS 64]
+long_mode_start:
+    ; Set up 64-bit segment registers
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov rsp, 0x200000       ; 64-bit stack
+
+    ; Load kernel from disk
+    call load_kernel_64
+
+    ; Jump to kernel entry point
+    mov rax, 0x100000       ; Kernel entry point
+    jmp rax
