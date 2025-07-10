26e08e987ec6dfdc272594f1035672ee9b4ba393
X4X0 <craxterpy@gmail.com>
2025-07-10 13:36:04 -0400

    Update gdt.asm

--- bootloader/gdt.asm
+++ bootloader/gdt.asm
@@ -1,1 +1,20 @@
+[BITS 16]
 
+; --------------------------------------
+; GDT - Global Descriptor Table
+; --------------------------------------
+
+gdt_start:
+    dq 0x0000000000000000     ; Null descriptor
+
+    ; Code Segment: base=0x00000000, limit=0xFFFFFFFF, type=0x9A
+    dq 0x00CF9A000000FFFF
+
+    ; Data Segment: base=0x00000000, limit=0xFFFFFFFF, type=0x92
+    dq 0x00CF92000000FFFF
+
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1    ; Size of GDT
+    dd gdt_start                  ; Linear address of GDT
