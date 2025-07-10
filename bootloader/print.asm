01c21db23082bbb25e665c1c171f9231148c4ef3
X4X0 <craxterpy@gmail.com>
2025-07-10 13:29:20 -0400

    Update print.asm

--- bootloader/print.asm
+++ bootloader/print.asm
@@ -1,1 +1,16 @@
+[BITS 16]
 
+print_string:
+    pusha
+.loop:
+    lodsb
+    test al, al
+    jz .done
+    mov ah, 0x0E
+    mov bh, 0x00
+    mov bl, 0x07
+    int 0x10
+    jmp .loop
+.done:
+    popa
+    ret
