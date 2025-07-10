6a4f43b3ef5e2527b880c7bb0a5b801976c360db
X4X0 <craxterpy@gmail.com>
2025-07-10 13:30:47 -0400

    Update stage1.asm

--- bootloader/stage1.asm
+++ bootloader/stage1.asm
@@ -1,1 +1,29 @@
+[BITS 16]
+[ORG 0x7C00]
 
+extern shark_animation
+extern print_string
+extern load_stage2
+
+boot_start:
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov sp, 0x7C00
+    
+    mov ax, 0x0003
+    int 0x10
+
+    call shark_animation
+    
+    mov si, boot_msg
+    call print_string
+    
+    call load_stage2
+    
+    jmp 0x1000:0x0000
+
+boot_msg: db 13,10, 'Booting SharkOS...', 13,10, 0
+times 510-($-$$) db 0
+dw 0xAA55
