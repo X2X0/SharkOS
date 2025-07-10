836168d02dc4625c4ad3bae9294ae4c3b6ccc82f
X4X0 <craxterpy@gmail.com>
2025-07-10 14:01:30 -0400

    Update gdt.asm

--- bootloader/gdt.asm
+++ bootloader/gdt.asm
@@ -1,15 +1,13 @@
 [BITS 16]
 
-; --------------------------------------
-; GDT - Global Descriptor Table
-; --------------------------------------
-
+; 32-bit GDT for protected mode
 gdt_start:
-    dq 0x0000000000000000     ; Null descriptor
-
+    ; Null descriptor
+    dq 0x0000000000000000
+    
     ; Code Segment: base=0x00000000, limit=0xFFFFFFFF, type=0x9A
     dq 0x00CF9A000000FFFF
-
+    
     ; Data Segment: base=0x00000000, limit=0xFFFFFFFF, type=0x92
     dq 0x00CF92000000FFFF
 
@@ -18,3 +16,20 @@ gdt_end:
 gdt_descriptor:
     dw gdt_end - gdt_start - 1    ; Size of GDT
     dd gdt_start                  ; Linear address of GDT
+
+; 64-bit GDT for long mode
+gdt64_start:
+    ; Null descriptor
+    dq 0x0000000000000000
+    
+    ; Code segment (64-bit)
+    dq 0x00AF9A000000FFFF
+    
+    ; Data segment (64-bit)
+    dq 0x00AF92000000FFFF
+
+gdt64_end:
+
+gdt64_descriptor:
+    dw gdt64_end - gdt64_start - 1
+    dd gdt64_start
